f351034baa92b3dc648a6b647af6585141a059d2
Christian-Silvestri <christiansilvestri@live.it>
2022-07-11 12:15:26 +0200

    props-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export class Welcome extends React.Component {
-    constructor(props) {
-        super(props)
-        this.name = props;
-    }
+ 
     render() {
-        return <p>Welcome, {this.props.name} !</p>
+        return (
+            <div>
+                <p>Welcome, {this.props.name} !</p>
+                <p>Your Age is {this.props.age}</p>
+            </div>
+        )
     }
 }
 
