6291b47a9fc37e3cd6f1b6f7c315a65a34aab506
Christian Silvestri <christiansilvestri@live.it>
2022-07-29 16:33:04 +0200

    context-02

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -1,10 +1,16 @@
 import React, { createContext } from 'react'
- 
+
 export const LanguageContext = createContext('en')
 
- export class DisplayLanguage extends React.Component {
-	render() {
-		return <div></div>
-	}
+export class DisplayLanguage extends React.Component {
+    
+    render() {
+
+        return (
+            <LanguageContext.Consumer>
+                {(language) => <h1>{language}</h1>}
+            </LanguageContext.Consumer>
+        )
+    }
 }
 
