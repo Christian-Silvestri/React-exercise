f26c4180c55de5ebc2e37c58d2682bf4e0705b85
Christian Silvestri <christiansilvestri@live.it>
2022-08-03 14:20:35 +0200

    react-router-03

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import Welcome from './Welcome'
 import ClickCounter from './ClickCounter'
+import GithubUser from './GitHubUser'
+
+function ShowGithubUser() {
+	const { username } = useParams()
+	return <GithubUser username={username} />
+}
 
 function AppRouter() {
 
@@ -9,6 +16,7 @@ function AppRouter() {
         <Routes>
             <Route path="/" element={<Welcome name="Jimmy" />} />
             <Route path="/counter" element={<ClickCounter />} />
+            <Route path="/users/:username" element={<ShowGithubUser />} />
         </Routes>
     )
 
