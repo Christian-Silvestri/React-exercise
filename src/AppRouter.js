b2ca576fa9fe36d15e29cf3fcf3b6bca14223230
Christian Silvestri <christiansilvestri@live.it>
2022-08-03 15:06:32 +0200

    react-router-07

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -19,6 +19,7 @@ function AppRouter() {
                 <Route path="/counter" element={<ClickCounter />} />
                 <Route path="/users" element={<GitHubUserList />}>
                     <Route path="/users/:username" element={<ShowGithubUser />} />
+                    <Route index element={<p>Add a user and select it</p>} />
                 </Route>
                 <Route path="*" element={<p>Not Found</p>} />
             </Routes>
