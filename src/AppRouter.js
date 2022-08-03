1a721c327b9bc10706b8fdca5a819c4f7c1c0ad6
Christian Silvestri <christiansilvestri@live.it>
2022-08-03 14:37:04 +0200

    react-router-04

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,23 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import {useParams} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Welcome from './Welcome'
 import ClickCounter from './ClickCounter'
 import GithubUser from './GitHubUser'
 
 function ShowGithubUser() {
-	const { username } = useParams()
-	return <GithubUser username={username} />
+    const { username } = useParams()
+    return <GithubUser username={username} />
 }
 
 function AppRouter() {
 
     return (
-
-        <Routes>
-            <Route path="/" element={<Welcome name="Jimmy" />} />
-            <Route path="/counter" element={<ClickCounter />} />
-            <Route path="/users/:username" element={<ShowGithubUser />} />
-        </Routes>
+        <div>
+            <Routes>
+                <Route path="/" element={<Welcome name="Jimmy" />} />
+                <Route path="/counter" element={<ClickCounter />} />
+                <Route path="/users/:username" element={<ShowGithubUser />} />
+            </Routes>
+            <Link to="/"><p>Home page</p></Link>
+            <Link to="/counter"><p>Counter</p></Link>
+            <Link to="/users/christiansilvestri"><p>Christian Silvestri</p></Link>
+        </div>
     )
 
 }
