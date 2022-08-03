5b8ddecab0564a225c5fa274591ea75f0b8598bf
Christian Silvestri <christiansilvestri@live.it>
2022-08-03 12:20:30 +0200

    custom-hooks-04

--- src/useGitHubUser.js
+++ src/useGitHubUser.js
@@ -1,18 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 function useGithubUser({ username }) {
     const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        return fetch(`https://api.github.com/users/${username}`)
+
+     async function fetchGithubUser(username) {
+
+        setIsLoading(true)
+
+        return await fetch(`https://api.github.com/users/${username}`)
             .then((response) => response.json())
-            .then((data) => setData(data));
-    }, [username])
+            .then((data) => setData(data))
+            .catch((error) => setError(error))
+            .finally(() => setIsLoading(false));
+    }
 
     return {
-        login: data.login,
-        url: data.url
+        data,
+        error,
+        isLoading,
+        User: fetchGithubUser
     }
 }
 
 export default useGithubUser
+
+
+
