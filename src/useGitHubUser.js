cc141752c9422186f63973d36632fbdd2459a3f7
Christian Silvestri <christiansilvestri@live.it>
2022-08-21 20:29:19 +0200

    swr-03

--- src/useGitHubUser.js
+++ src/useGitHubUser.js
@@ -3,12 +3,17 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((response) => response.json())
 
 function useGithubUser(username) {
-    const { data, error} = useSWR( username ? `https://api.github.com/users/${username}` : null, fetcher)
+    const { data, error, mutate} = useSWR( username ? `https://api.github.com/users/${username}` : null, fetcher)
+
+    function handleRefetch() {
+        mutate()
+    }
 
     return {
         users: data,
         error,
-        isLoading: !data && !error
+        isLoading: !data && !error,
+        onRefetch: handleRefetch
     }
 }
 
